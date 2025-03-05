265f62868c74875c8e9936493d1df1eaafb02d54
nduc <48994241+nduc03@users.noreply.github.com>
2025-03-05 21:16:19 +0700

    add renameFile to RemoteFileSync interface

--- RemoteFileSync.h
+++ RemoteFileSync.h
@@ -9,6 +9,7 @@ public:
 	virtual void uploadFile(std::string localPath, std::string remotePath) = 0; // push
 	virtual void downloadFile(std::string remotePath, std::string localPath) = 0; // pull
 	virtual void deleteFile(std::string remotePath) = 0; // rm
+	virtual void renameFile(std::string remotePath, std::string newName) = 0; // mv
 
 	virtual std::vector<std::string> fetchFileList(std::string remotePath) = 0; // fetch
 
